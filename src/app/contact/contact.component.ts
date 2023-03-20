f76210114b290ffbd92307bbe04a1fb0498c5f78
thenyachhon <63194470+TheNyachhon@users.noreply.github.com>
2023-03-20 10:33:17 +0530

    rough contact us page

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
-
+  // images
+  images = [
+    "./assets/AerialViewOfKTM.jpg",
+    "./assets/boudhaStupa.jpg",
+    "./assets/boudhaStupa2.jpg",
+    "./assets/candles.jpg",
+    "./assets/mangalbazar.jpg",
+    "./assets/mangalbazar2.jpg",
+    "./assets/mangalbazar3.jpg",
+    "./assets/mangalbazar4.jpg",
+    "./assets/mangalbazar5.jpg",
+    "./assets/mangalbazar6.jpg",
+    "./assets/NationalParkLangtang.jpg",
+    "./assets/pashupati-aarti.jpg",
+    "./assets/pokhara-phewataal.jpg",
+    "./assets/lakhey.jpg",
+    "./assets/lakhey2.jpg",
+    "./assets/lumbini.jpg",
+    "./assets/janakpur2.jpg",
+    "./assets/chitwan.jpg",
+    "./assets/tihar1.jpg",
+    "./assets/tihar2.jpg",
+    "./assets/tihar3.jpg",
+    "./assets/indrajatra.jpg",
+    "./assets/dashain.jpg",
+    "./assets/gaijatra.jpg",
+    "./assets/gaijatra2.jpg",
+    "./assets/mhapuja.jpg",
+    "./assets/yomari.jpg"
+  ]
 }
